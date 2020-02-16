a2ee97441735604c164857eeb2f70a2329edbc29
jeffreydahan <43243412+jeffreydahan@users.noreply.github.com>
2020-02-16 12:04:58 +0200

    ssh key work

--- aks-deploy.sh
+++ aks-deploy.sh
@@ -37,7 +37,8 @@ echo $subnetidaks
 # create aks cluster
 az aks create --resource-group $rgname --name $clusternameaks --enable-cluster-autoscaler \
   --node-count $nodecountaks --min-count $minnodecountaks --max-count $maxnodecountaks \
-  --network-plugin $networkpluginaks --vnet-subnet-id $subnetidaks --zones $zonesaks 
+  --network-plugin $networkpluginaks --vnet-subnet-id $subnetidaks --zones $zonesaks \
+  --no-ssh-key
 
 # validate nodes
 az aks get-credentials --resource-group $rgname --name $clusternameaks
